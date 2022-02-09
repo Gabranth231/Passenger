1493435aef1163e68d440fb8dff1fc57b8875439
MARTIN BOYCE - STUDENT <G00368112@gmit.ie>
2022-02-09 09:14:27 +0000

    First Files

---
+++ src/main/java/programTest/gmit/Passenger.java
@@ -0,0 +1,10 @@
+package programTest.gmit;
+public class Passenger {
+    private String Title;
+    private String Name;
+    private String ID;
+    private String PhoneNum;
+    private int Age;
+
+
+}
